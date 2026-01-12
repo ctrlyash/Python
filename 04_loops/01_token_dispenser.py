9b6ee515a7dc3e963626135cdace258c10f36bf0
Yash Dadhich <yashdadhich9934@gmail.com>
2026-01-12 23:40:48 +0530

    add loops in python task 1

---
+++ 04_loops/01_token_dispenser.py
@@ -0,0 +1,17 @@
+# A tea stall owner has a digital token display. For every customer in line, a token number is printed and chai is served.
+# Task:
+# • Use a for loop to generate token numbers from 1 to 10 using range() • Print: "Serving chai to Token #[number]"
+
+for token in range(1, 11):
+    print(f"Serving chai to Token #{token}")
+
+# Serving chai to Token #1
+# Serving chai to Token #2
+# Serving chai to Token #3
+# Serving chai to Token #4
+# Serving chai to Token #5
+# Serving chai to Token #6
+# Serving chai to Token #7
+# Serving chai to Token #8
+# Serving chai to Token #9
+# Serving chai to Token #10    
