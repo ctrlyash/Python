e3e4f1da3891bc302d2115e4c9aff39b9c43d3ed
Yash Dadhich <yashdadhich9934@gmail.com>
2026-01-12 23:41:31 +0530

    add loops in python task 2

---
+++ 04_loops/02_tea_batch.py
@@ -0,0 +1,13 @@
+# A chai shop makes tea in batches every 15 minutes.
+# You want to simulate 4 batches.
+# Task:
+# • Use range() to simulate batch numbers.
+# • Print: "Preparing chai for batch #[number]
+
+for batch in range(1, 5):
+    print(f"Preparing chai for batch #{batch}")
+
+# Preparing chai for batch #1
+# Preparing chai for batch #2
+# Preparing chai for batch #3
+# Preparing chai for batch #4
