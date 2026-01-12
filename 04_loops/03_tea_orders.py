362f2a9a194d945cbc1305e9801bd60214c05f6a
Yash Dadhich <yashdadhich9934@gmail.com>
2026-01-12 23:42:02 +0530

    add loops in python task 3

---
+++ 04_loops/03_tea_orders.py
@@ -0,0 +1,15 @@
+# You receive a list of names for chai orders. The goal is to print out the order queue. 
+# Task:
+# • Use a list of names.
+# • Print: "Order ready for [name]"
+
+orders = ["Rahul", "Yash", "Siddharth", "Virat", "Rohit"]
+
+for name in orders:
+    print(f"Order ready for {name}")
+
+#Order ready for Rahul
+# Order ready for Yash
+# Order ready for Siddharth
+# Order ready for Virat
+# Order ready for Rohit    
