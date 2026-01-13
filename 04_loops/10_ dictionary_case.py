321c6a4b3f301e3b5e3d9f00f3b701adaa37d991
Yash Dadhich <yashdadhich9934@gmail.com>
2026-01-13 23:56:40 +0530

    add dictionary case in python

---
+++ 04_loops/10_ dictionary_case.py
@@ -0,0 +1,25 @@
+ # Using Dictionaries Instead of Repeated Cases
+
+users = [
+    {"id": 1, "total": 100, "coupon": "P20"},
+    {"id": 2,  "total": 80, "coupon": "F10"},
+    {"id": 3,  "total": 150, "coupon": "P50"}
+]
+
+
+discounts = {
+    "P20": (0.2, 0),
+    "F10": (0.5, 0),
+    "P50": (0, 10)
+}
+
+
+for user in users:
+    percent, fixed = discounts.get(user["coupon"], (0, 0))
+    discount = user["total"] * percent + fixed
+    print(f"{user["id"]} paid {user["total"]} and got discount for next visit of rupees {discount}")
+
+
+# 1 paid 100 and got discount for next visit of rupees 20.0
+# 2 paid 80 and got discount for next visit of rupees 40.0
+# 3 paid 150 and got discount for next visit of rupees 10
