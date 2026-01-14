b84bfc2c3f2a0123e835b3dfb494d34833f731bd
Yash Dadhich <yashdadhich9934@gmail.com>
2026-01-14 11:20:16 +0530

    add functions in python task 2

---
+++ 05_functions/02_complex.py
@@ -0,0 +1,35 @@
+# Splitting Complex Tasks
+
+# You're creating a monthly report for a cafe's sales.
+# Instead of putting all logic in one place, break it down.
+# Task
+# Write a function generate_report() that calls:
+# fetch_sales()
+# fetch_valid_orders()
+# summarize_data()
+
+def fetch_sales():
+    print("Fetching sales data...")
+
+
+def filter_valid_sales():
+    print("Filtering valid sales data...")
+
+
+def summarize_data():
+    print("Summarizing sales data...")
+
+
+def generate_report():
+    fetch_sales()
+    filter_valid_sales()
+    summarize_data()
+    print("Report is ready!")
+
+generate_report()    
+
+
+# Fetching sales data...
+# Filtering valid sales data...
+# Summarizing sales data...
+# Report is ready!
