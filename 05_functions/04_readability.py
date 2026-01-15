2cf88a6440096f075bcbb04e4e8ab50fe7c8a393
Yash Dadhich <yashdadhich9934@gmail.com>
2026-01-15 23:43:56 +0530

    add functions in python task 4

---
+++ 05_functions/04_readability.py
@@ -0,0 +1,19 @@
+# Improving Readability
+
+# You sell different chai sizes.
+# Instead of writing formulas everywhere, create a function.
+# Task:
+# - Write calculate_bill(cups, price_per_cup)
+# - Return total bill
+# - Use this function for multiple orders
+
+
+def calculate_bill(cups, price_per_cup):
+    return cups * price_per_cup
+
+
+my_bill =calculate_bill(3, 15)
+print(my_bill) # 45
+
+print("Order for table 2: ", calculate_bill(2, 50))
+# Order for table 2:  100
