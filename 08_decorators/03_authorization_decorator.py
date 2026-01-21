9faf13965112dde085a1913aa60bed619a4008bc
Yash Dadhich <yashdadhich9934@gmail.com>
2026-01-21 22:21:50 +0530

    add more about decorators in python

---
+++ 08_decorators/03_authorization_decorator.py
@@ -0,0 +1,24 @@
+from functools import wraps
+
+def require_admin(func):
+    @wraps(func)  #(preseves the metadata)
+    def wrapper(user_role):
+        if user_role != "admin":
+            print("Access denied: Admins only")
+            return None # optional 
+        else:
+            return func(user_role)  
+    return wrapper
+
+@require_admin
+def access_tea_inventory(role):
+    print("Access granted to tea inventory")
+
+access_tea_inventory("user")
+access_tea_inventory("admin")
+
+# Access denied: Admins only
+# Access granted to tea inventory
+
+
+
